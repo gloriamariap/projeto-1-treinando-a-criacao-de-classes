382898e0d81f388fb8d5325852934cd2b38f5da8
Glória Maria <160123526+gloriamariap@users.noreply.github.com>
2024-08-14 08:20:51 -0300

    Update Product.js

--- Product.js
+++ Product.js
@@ -8,25 +8,24 @@ class Product {
 
     addToStock(quantity) {
         this.inStock += quantity
+        console.log(`
+             Nome do produto: ${this.name}
+             Descrição do produto: ${this.description}
+             Valor do produto${this.price.tofixed(2)}
+             Quantidade do produto: ${this.inStock}
+        `)
     }
 
-    calculateDiscount(discountPercentage) {
-        const discountAmount = (this.price * discountPercentage) / 100
-        return this.price - discountAmount
-    }
+    calculateDiscount(discount) {
+        this.price = this.price - (discount * this.price / 100) 
+        console.log(` 
+          ===========================
+          Desconto no produto: ${discount}%
+          Novo valor do produto: ${this.price.toFixed(2)}
+        `)  
+  }
 }
 
-const product = new Product('Notebook', 'Notebook Nitro 7', '2000')
-product.addToStock (10)
-
-console.log(`Nome do produto: ${product.name}`)
-console.log(`Descrição do produto: ${product.description}`)
-console.log(`Valor do produto: R$${product.price}.00`)
-console.log(`Quantidade no estoque: ${product.inStock}`)
-console.log('=====================================')
-
-const discount = 10
-const discountedPrice = product.calculateDiscount(discount)
-
-console.log(`Desconto no produto: ${discount}%`)
-console.log(`Novo valor do produto: R$${discountedPrice}.00`)
+const notebook = new Product('Notebook', 'Notebook Nitro 7', '2000')
+notebook.addToStock(10)
+notebook.calculateDiscount(5)
